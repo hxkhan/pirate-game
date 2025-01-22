4198ba36093539c6016eece1f6305f3dbb28f77c
hxkhan <mail@hxkhan.dev>
2025-01-22 21:09:02 +0100

    Better Overlay

--- World.gd
+++ World.gd
@@ -3,16 +3,16 @@ extends Node
 var show_debug: bool = true
 
 func _process(_delta: float) -> void:
-	$Debug.text = "FPS: " + str(Engine.get_frames_per_second())
-	$Debug.text += "\nSpeed: " + str(round($Player.speed))
-	$Debug.text += "\nTurn Speed: " + str(round($Player.turn_radius))
+	$Overlay/Debug.text = "FPS: " + str(Engine.get_frames_per_second())
+	$Overlay/Debug.text += "\nSpeed: " + str(round($Player.speed))
+	$Overlay/Debug.text += "\nTurn Speed: " + str(round($Player.turn_radius))
 
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_P:
 			if show_debug:
-				$Debug.hide()
+				$Overlay/Debug.hide()
 				show_debug = false
 			else:
-				$Debug.show()
+				$Overlay/Debug.show()
 				show_debug = true
