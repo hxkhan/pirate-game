e7d590193d09af716ff4ae1f981ae42b343ac1de
hxkhan <mail@hxkhan.dev>
2025-02-12 21:23:18 +0100

    Bullet Logic & Collisions

--- CannonBall.gd
+++ CannonBall.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-var shot_dir : Vector2 
+var shot_dir : Vector2
+var shooter : CharacterBody2D
 var speed = 300
 
 var explosion_scene = preload("res://PNG/Default size/Effects/explosion3.png")
@@ -22,5 +23,8 @@ func on_explosion_timer_timeout() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	$Timer.stop()
-	_on_timer_timeout()
+	if body == shooter:
+		return
+	if body is CharacterBody2D or body.name == "Dock":
+		$Timer.stop()
+		_on_timer_timeout()
