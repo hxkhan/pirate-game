8632bc03ae6b2d950503797a5ab4d6065ed5c766
davidgol <davidgol@chalmers.se>
2025-03-10 13:36:50 +0100

    Respawnable Dummy

--- TutorialDummy.gd
+++ TutorialDummy.gd
@@ -5,14 +5,20 @@ signal dead_dummy()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if health <= 0:
 		dead_dummy.emit(1, global_position)
-		queue_free()
+		visible = false
+		health = 100
+		$Timer.start()
 
 func _on_area_entered(area: Area2D) -> void:
 	health -= 25
+
+
+func _on_timer_timeout() -> void:
+	visible = true
