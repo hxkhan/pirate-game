dbb30a683f3a3d94eae69938e13a37a85e337985
davidgol <davidgol@chalmers.se>
2025-03-10 12:35:54 +0100

    Added dummy fixes

--- TutorialDummy.gd
+++ TutorialDummy.gd
@@ -1,6 +1,35 @@
 extends CharacterBody2D
 
-var health = 100
+@export var skin: String = "WHITE":
+	set(value):
+		skin = value
+		$Sprite.texture = load(Globals.skin_names[skin][0])
 
-func _physics_process(delta: float) -> void:
-	pass
+@export var max_health: int = 100
+@export var health: int = 100:
+	set(value):
+		health = clamp(value, 0, max_health)
+		var percent = (float(health) / max_health) * 100
+		var skin_dir = ""
+		
+		if percent > 0 and $CollisionShape.disabled:
+			$CollisionShape.set_deferred("disabled", false)
+		
+		if percent <= 0:
+			skin_dir = Globals.skin_names[skin][3]
+			$CollisionShape.set_deferred("disabled", true)
+		elif percent <= 25:
+			skin_dir = Globals.skin_names[skin][2]
+		elif percent <= 75:
+			skin_dir = Globals.skin_names[skin][1]
+		else:
+			skin_dir = Globals.skin_names[skin][0]
+		
+		$Sprite.texture = load(skin_dir)
+
+func is_dead():
+	return health <= 0
+
+
+func _on_collision_shape_2d_body_entered(body: Node2D) -> void:
+	health -= 25
